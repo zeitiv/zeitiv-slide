46ebd18b717132b193437aee587ba69a811e5e5b
zeitiv <24879428+zeitiv@users.noreply.github.com>
2024-07-19 09:33:28 +0200

    realease

---
+++ setup/shiki.ts
@@ -0,0 +1,14 @@
+/* ./setup/shiki.ts */
+import { defineShikiSetup } from '@slidev/types'
+
+export default defineShikiSetup(() => {
+  return {
+    themes: {
+      dark: 'catppuccin-mocha',
+      light: 'catppuccin-mocha',
+    },
+    transformers: [
+      // ...
+    ],
+  }
+})
--- theme/layouts/section.vue
+++ theme/layouts/section.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slidev-layout section w-full h-full grid">
-    <div class="my-auto text-center">
+    <div class="my-auto text-center bg-green">
       <slot />
     </div>
   </div>
